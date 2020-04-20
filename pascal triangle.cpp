c99cca4aa3e50c365c755a7a21dbbc51112637f4
Prajwal Singh <prajwal714singh@gmail.com>
2020-04-20 18:53:23 +0000

    fixed pascals triangle

--- pascal triangle.cpp
+++ pascal triangle.cpp
@@ -4,24 +4,44 @@ using namespace std;
 int main()
 {
 	int row;
+    cout<<"Enter Number of Row: "<<endl;
 	cin>>row;
 	
-	vector<vector<int>> arr(row);
-    arr[0].push_back(1);
+	int arr[row][row];
     
-    int i;
-    for(i=1;i<row;i++)
+    
+    
+
+    int i,j;
+
+    for(i=0;i<row;i++)
     {
-        arr[i].push_back(1);
-        for(int j=1;j<i+1;j++)
+        for(j=0;j<=i;j++)
         {
-            if(j==i)
-            arr[i].push_back(1);
-            else
-            arr[i].push_back(arr[i-1][j]+arr[i-1][j-1]);
+            if(i==j || j==0)
+            arr[i][j]=1;
+           
         }
     }
-    for(i=0;i<row)
+    for(i=1;i<row;i++)
+    {
+        for(j=1;j<i;j++)
+        {
+            arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+        }
+    }
+
+     for(i=0;i<row;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+           cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
 }
 
 
