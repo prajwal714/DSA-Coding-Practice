f72969518be7281dc457ec61721399168d30187d
prajwal714 <prajwal714singh@gmail.com>
2020-04-24 15:26:01 +0530

    Edit distance

--- GeeksForGeeks/DP/edit distance.cpp
+++ GeeksForGeeks/DP/edit distance.cpp
@@ -1,10 +1,37 @@
 #include<iostream>
 #include<bits/stdc++.h>
-
-//make string s1==s2 by either removind,adding or replacing characters and minimize the editing
 using namespace std;
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+
+int editCost(string s1, string s2, int m, int n)
+{
+    int dp[m+1][n+1];
+    
+    int i,j;
+    for(i=0;i<=m;i++)
+    {
+        for(j=0;j<=n;j++)
+        {
+            if(i==0||j==0)
+            dp[i][j]=max(i,j);
+            
+            else if(s1[i-1]==s2[j-1])
+            dp[i][j]=dp[i-1][j-1];
+            
+            else
+            
+            dp[i][j]=min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]))+1;
+        }
+    }
+    
+    return dp[m][n];
+}
+
+
 int main()
  {
+    IOS;
+	//code
 	int t;
 	cin>>t;
 	while(t--)
@@ -14,27 +41,7 @@ int main()
 	    string s1,s2;
 	    cin>>s1>>s2;
 	    
-	    int dp[m+1][n+1];
-	    int i,j;
-	    for(i=0;i<=m;i++)
-	    {
-	        for(j=0;j<=n;j++)
-	        {
-	            if(i==0||j==0)
-	            dp[i][j]=max(i,j);
-	            
-	            else if(s1[i-1]==s2[j-1])
-	            {
-	                dp[i][j]=dp[i-1][j-1];
-	            }
-	            else
-	            dp[i][j]=min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
-	        }
-	    }
-	    
-	   
-	    cout<<dp[m][n]<<endl;
-	  
+	    cout<<editCost(s1,s2,m,n)<<endl;
 	    
 	}
 	return 0;
